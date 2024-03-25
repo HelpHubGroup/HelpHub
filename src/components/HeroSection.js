4f26cd80dcfdb65826d4fd11b47179fb4d589049
kevinsu-rgb <116765823+kevinsu-rgb@users.noreply.github.com>
2024-03-25 00:41:58 -0400

    Created local server for connection to MongoDB

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -1,9 +1,24 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import '../App.css';
 import { Button } from './Button';
 import './HeroSection.css';
+import axios from 'axios';
 
 function HeroSection() {
+  const [items, setItems] = useState([]);
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const response = await axios.get('http://localhost:5001/api/items');
+        console.log('Response:', response.data); // Log the response data to the console
+        setItems(response.data);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    }
+    fetchData();
+  }, []);
+
   return (
     <div className = 'hero-container'>
      <div className='input-areas'>
