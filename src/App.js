90eb0b9ecdf5d1234cf9c1935b7546bd7f4dad5b
MalloryRose <116680613+MalloryRose@users.noreply.github.com>
2024-03-25 19:53:10 -0400

    added login page
    added login page

--- src/App.js
+++ src/App.js
@@ -6,8 +6,14 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Services from './components/pages/Services';
 import Products from './components/pages/Products';
 import SignUp from './components/pages/SignUp';
+import { useEffect, useState } from 'react'
 
 function App() {
+  // initialize the variables for user login
+  // and email globally
+  const [loggedIn, setLoggedIn] = useState(false)
+  const [email, setEmail] = useState('')
+
   return (
     <>
       <Router>
@@ -16,7 +22,7 @@ function App() {
         <Route path='/' exact element={ <Home />}/>
           <Route path='/services' element={<Services />} /> 
           <Route path='/products' element={<Products />} />
-          <Route path='/sign-up' element={<SignUp />} />
+          <Route path='/sign-up' element={<SignUp setLoggedIn={setLoggedIn} setEmail={setEmail}    />} />
         </Routes>
       </Router>
     </>
