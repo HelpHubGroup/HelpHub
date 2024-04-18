bc2c75bc9c8a7034c87b942966e57479eed8b93b
jacobpenas <92338651+jacobpenas@users.noreply.github.com>
2024-04-18 15:37:47 -0400

    Add files via upload

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -1,62 +1,40 @@
-import React, { useState, useEffect } from 'react';
-import '../App.css';
-import { Button } from './Button';
 import './HeroSection.css';
+import React, { useState } from 'react';
+import '../App.css';
 import axios from 'axios';
 
-
-function HeroSection({setResults}) {
+function HeroSection({ onSearch }) {
   const [query, setQuery] = useState('');
-  const [items, setItems] = useState([]);
-  
+  const [searching, setSearching] = useState('');
 
   const handleInputChange = (event) => {
     setQuery(event.target.value);
   };
 
-  useEffect(() => {
-    if (items.length > 0) {
-      console.log(items[0].Item_Name);
-
-      
-
-    }
-  }, [items]);
-
-  const handleSubmit = async (event) => {
-    event.preventDefault();
+  const handleSearch = async () => {
+    setSearching(true);
     try {
       const response = await axios.get(`http://localhost:5001/api/getitems?query=${query}`);
-   
-      console.log(response.data);
-      setItems(response.data);
-      setResults(response.data);
+      onSearch(response.data);
     } catch (error) {
       console.error('Error fetching data:', error);
+    } finally {
+      setSearching(false);
     }
   };
 
   return (
     <div className='hero-container'>
       <div className='input-areas'>
-        <form onSubmit={handleSubmit}>
-          <input
-            className='hero-input'
-            placeholder='SEARCH'
-            value={query}
-            onChange={handleInputChange}
-          />
-          <Button type="submit" className='hero-search-icon' destination={'/product-view'}>
-           
-           
-            <i className="fas fa-search" />
-          </Button>
-          
-        </form>
-      </div>
-      <div className ='data-result'>
-
-
+        <input
+          className='hero-input'
+          placeholder='SEARCH'
+          value={query}
+          onChange={handleInputChange}
+        />
+        <button className='hero-search-icon' onClick={handleSearch}>
+          <i className="fas fa-search" />
+        </button>
       </div>
     </div>
   );
